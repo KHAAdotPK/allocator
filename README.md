5418d0408266ec4df97a2cad5aaebfe3714fa927
KHAAdotPK <spysee.pk@gmail.com>
2023-12-31 20:44:19 +0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# allocator
-The `allocator` class provided in this repository is a template class that specializes in allocating memory for different data types.
+# C++ allocator class
+
+## Overview
+
+The `allocator` class provided in this repository is part of the `cc_tokenizer` namespace in C++. It is a template class that specializes in allocating memory for different data types. The allocator supports several predefined types, including `char`, `float`, `double`, `unsigned char`, `int`, `wchar_t`, and `size_t`. 
+
+## Usage:-
+
+### Template Specializations
+
+The allocator is specialized for different data types, and each specialization provides the following public typedefs:
+
+- `value_type`: The type of elements to allocate.
+- `pointer`: A pointer to `value_type`.
+- `const_pointer`: A const pointer to `value_type`.
+- `reference`: A reference to `value_type`.
+- `const_reference`: A const reference to `value_type`.
+- `size_type`: An unsigned integer type representing the size of allocated memory.
+- `difference_type`: A signed integer type representing the difference between two pointers.
+
+### Member Functions
+
+1. **max_size():**
+   - Returns the maximum number of elements that the allocator can allocate.
+
+2. **allocate(size_type n):**
+   - Allocates memory for `n` elements of type `value_type`.
+   - Throws `std::bad_alloc` if the allocation fails.
+   - Throws `std::length_error` if the requested allocation size exceeds the maximum size.
+
+3. **deallocate(pointer ptr, size_type n = static_cast<size_type>(0)):**
+   - Deallocates the memory pointed to by `ptr`.
+   - The optional parameter `n` is ignored and included for compatibility with the standard allocator interface.
+
+## Example
+
+```cpp
+#include <iostream>
+#include "./../lib/allocator/allocator.hh"
+
+using cc_tokenizer::allocator;
+
+int main(int argc, char* argv[]) {
+
+    allocator<size_t>::pointer ptr;
+    
+    try {
+        ptr = allocator<size_t>().allocate(19);
+        allocator<size_t>().deallocate(ptr);
+    }
+    catch (std::bad_alloc& e) {
+        std::cout<< e.what() << std::endl;
+    }
+    
+    return 0;
+}
+```
+### License
+This project is governed by a license, the details of which can be located in the accompanying file named 'LICENSE.' Please refer to this file for comprehensive information.
---
+++ allocator.hh
@@ -0,0 +1,453 @@
+/* 
+   src/allocator.hh
+   Written by, Q@khaa.pk
+ */
+
+#include<stdexcept>
+#include<stdlib.h>
+
+/* Include guards */
+#ifndef CC_TOKENIZER_ALLOCATOR_HH
+#define	CC_TOKENIZER_ALLOCATOR_HH
+
+namespace cc_tokenizer {
+
+template <typename E>
+class allocator;
+
+template <>
+class allocator<char> {
+
+   public:
+      typedef	char			         value_type;
+      typedef	value_type*		      pointer;
+      typedef	const value_type* 	const_pointer;
+      typedef	value_type&		      reference;
+      typedef	const value_type& 	const_reference;
+      typedef	std::size_t		      size_type;
+      typedef	std::ptrdiff_t	  	   difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+
+template <>
+class allocator<float> {
+
+   public:
+       typedef	float                value_type;
+       typedef	value_type*	         pointer;
+       typedef	const value_type* 	const_pointer;
+       typedef	value_type&		      reference;
+       typedef	const value_type& 	const_reference;
+       typedef	std::size_t		      size_type;
+       typedef	std::ptrdiff_t	  	   difference_type;
+
+       template<typename U> struct rebind {
+           typedef	allocator<U>	other;
+       };
+
+       size_type max_size(void) const {
+    
+           return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+       }
+
+       pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }        
+};
+
+template <>
+class allocator<double> {
+
+   public:
+       typedef	double               value_type;
+       typedef	value_type*	         pointer;
+       typedef	const value_type* 	const_pointer;
+       typedef	value_type&		      reference;
+       typedef	const value_type& 	const_reference;
+       typedef	std::size_t		      size_type;
+       typedef	std::ptrdiff_t	  	   difference_type;
+
+       template<typename U> struct rebind {
+           typedef	allocator<U>	other;
+       };
+
+       size_type max_size(void) const {
+    
+           return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+       }
+
+       pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }        
+};
+
+template <>
+class allocator<unsigned char> {
+
+   public:
+      typedef	unsigned char			  value_type;
+      typedef	value_type*		        pointer;
+      typedef	const value_type* 	  const_pointer;
+      typedef	value_type&		        reference;
+      typedef	const value_type& 	  const_reference;
+      typedef	std::size_t		        size_type;
+      typedef	std::ptrdiff_t	  	     difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+
+template <>
+class allocator<int> {
+
+   public:
+      typedef	int			            value_type;
+      typedef	value_type*		         pointer;
+      typedef	const value_type* 	   const_pointer;
+      typedef	value_type&		         reference;
+      typedef	const value_type& 	   const_reference;
+      typedef	std::size_t		         size_type;
+      typedef	std::ptrdiff_t	  	      difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+
+/*
+template <>
+class allocator<unsigned int> {
+
+   public:
+      typedef	unsigned int			  value_type;
+      typedef	value_type*		        pointer;
+      typedef	const value_type* 	  const_pointer;
+      typedef	value_type&		        reference;
+      typedef	const value_type& 	  const_reference;
+      typedef	std::size_t		        size_type;
+      typedef	std::ptrdiff_t	  	     difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+*/
+
+template <>
+class allocator<wchar_t> {
+
+   public:
+      typedef	wchar_t			      value_type;
+      typedef	value_type*		      pointer;
+      typedef	const value_type* 	const_pointer;
+      typedef	value_type&		      reference;
+      typedef	const value_type& 	const_reference;
+      typedef	std::size_t		      size_type;
+      typedef	std::ptrdiff_t	  	   difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+
+template <>
+class allocator<size_t> {
+
+   public:
+      typedef	std::size_t			      value_type;
+      typedef	value_type*		         pointer;
+      typedef	const value_type* 	   const_pointer;
+      typedef	value_type&		         reference;
+      typedef	const value_type& 	   const_reference;
+      typedef	std::size_t		         size_type;
+      typedef	std::ptrdiff_t	  	      difference_type;
+
+      template<typename U> struct rebind {
+
+         typedef	allocator<U>	other;
+      };
+
+      size_type max_size(void) const {
+    
+         return (static_cast<size_type>(0) + static_cast<size_type>(-1)) / sizeof(value_type); 
+      }
+      
+      pointer allocate(const size_type n) const throw(std::bad_alloc, std::length_error) {
+      
+         void* ptr = NULL;
+      
+         if ( n > 0 ) {
+         
+            if ( n > max_size() ) {
+         
+               throw std::length_error("cc_tokenizer::allocator<>::allocate() - Integer overflow.");
+            }
+         
+            ptr = malloc( n * sizeof(value_type) );
+         
+            if ( ptr == NULL ) {
+         
+               throw std::bad_alloc();
+            }
+         
+         }
+        
+         return static_cast<pointer>(ptr); 
+      }
+       
+      void deallocate(pointer ptr, size_type n = static_cast<size_type>(0)) {
+       
+         if ( ptr ) {
+          
+            free(ptr);
+         } 
+      }      
+};
+
+};
+
+#endif
+
